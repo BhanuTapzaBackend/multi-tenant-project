3f02b9e2a261feac55c9fcf35cbc1466ec66d0ad
BhanuTapzaBackend <backend.intern@tapza.in>
2025-10-01 12:22:58 +0530

    Added Post for products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Product } from './product.schema';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductsService {
@@ -8,5 +9,10 @@ export class ProductsService {
 
   async getProducts() {
     return this.ProductModel.find();
+  }
+  // Add this new method
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const newProduct = new this.ProductModel(createProductDto);
+    return newProduct.save();
   }
 }
