7f3757f9633d270ba07ad4ad25adac07861e3af5
BhanuTapzaBackend <backend.intern@tapza.in>
2025-10-01 11:59:38 +0530

    Added the Api Properrty for dto

--- src/tenants/create-company.dto.ts
+++ src/tenants/create-company.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import UserDto from "src/users/user.dto";
 
 export default class CreateCompanyDto {
+  @ApiProperty()
   companyName: string;
+  @ApiProperty()
   user: UserDto
 }
